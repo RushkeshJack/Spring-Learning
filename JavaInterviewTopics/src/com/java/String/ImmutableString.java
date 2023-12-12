e23394cb8d172a51faa838f1ed611f37c22042a7
RushkeshJack <86036905+RushkeshJack@users.noreply.github.com>
2023-12-12 08:16:35 +0530

    immutable string in java

--- JavaInterviewTopics/src/com/java/String/ImmutableString.java
+++ JavaInterviewTopics/src/com/java/String/ImmutableString.java
@@ -6,6 +6,11 @@ public class ImmutableString {
 		
 		String name =  "Rushikesh";
 		
+		//In this case, new string object is getting created
+		//and reference to that string object is mapped to name variable.
+		//string object with content "Rushikesh" is now alone in heap
+		//(no one pointing to that location).
+		
 		name = "Devdatta";
 		
 		System.out.println(name);
